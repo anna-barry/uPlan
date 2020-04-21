c59b9f2c684e4e37bc81d14aad3d575b31b35a8e
anna.barry@me.com <anna.barry@me.com>
2020-04-21 13:23:45 +0200

    Shopping List Level 1 works

--- Uplan/UplanTest/UplanTest/Food/FoodW.xaml.cs
+++ Uplan/UplanTest/UplanTest/Food/FoodW.xaml.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using LiteDB;
 
 namespace UplanTest
 {
@@ -90,7 +91,42 @@ namespace UplanTest
 
             if (toggled)
             {
-                GetShoppinForWeek();
+                //Search s'ils ne sont pas déjà présent
+                //bool NotalreadySavede = true;
+                var col = Database.db.GetCollection<FoodItem>("FoodForShoppingList");
+
+                //Results 
+                //__________________________ Prot
+                var resultprot1 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeProt1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt1)));
+                bool prot1 = (resultprot1 == null);
+                var resultprot2 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeProt2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt2)));
+                bool prot2 = (resultprot2 == null);
+                var resultprot3 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeProt3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt3)));
+                bool prot3 = (resultprot3 == null);
+
+
+                //__________________________ Carb
+                var resultcarb1 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeCarb1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb1)));
+                bool carb1 = (resultcarb1 == null);
+                var resultcarb2 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeCarb2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb2)));
+                bool carb2 = (resultcarb2 == null);
+                var resultcarb3 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeCarb3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb3)));
+                bool carb3 = (resultcarb1 == null);
+
+                //___________________________ Veggies
+
+                var resultveggie1 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeVeggies1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies1)));
+                bool veggie1 = (resultveggie1 == null);
+                var resultveggie2 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeVeggies2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies2)));
+                bool veggie2 = (resultveggie2 == null);
+                var resultveggie3 = col.FindOne(Query.And(Query.EQ("Code", MyFoodWeek.thisweek.FoodCategoryCodeVeggies3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies3)));
+                bool veggie3 = (resultveggie3 == null);
+
+
+                //if(prot1 & prot2 & prot3 & carb1 & carb2 & carb3 & veggie1 & veggie2 & veggie3)
+               // {
+                    GetShoppinForWeek();
+               // }
             }
 
         }
@@ -108,63 +144,207 @@ namespace UplanTest
             //INSERT FOOD ITEM FOR EACH FOOD OF THIS WEEK
             // for the dueDate the idea is that for the next Sunday the food must be eaten (new food plan after that) 
             //-> we need to add the possibility to change all due Dates
+
             DateTime res = GetNextSunday();
 
+            //_______________________________________________________________
+            //_____________________ Carb ____________________________________
             string typeForCarb1 = MyFoodWeek.thisweek.FoodCategoryTypeCarb1;
             string codeForCarb1 = MyFoodWeek.thisweek.FoodCategoryCodeCarb1;
             string descForCarb1 = MyFoodWeek.thisweek.FoodCategoryDescCarb1;
-            FoodItem.InsertFoodItem(typeForCarb1, codeForCarb1, descForCarb1, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb1, codeForCarb1));
+            //Si n'est pas présent dans food item
+            var col2 = Database.db.GetCollection<FoodItem>("FoodItems");
+            var resultcarb1 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb1)));
+            bool carb1 = (resultcarb1.Count() == 0);
+            if(carb1)
+            {
+                FoodItem.InsertFoodItem(typeForCarb1, codeForCarb1, descForCarb1, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var rescarb1 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb1)));
+            bool shcarb1 = (rescarb1.Count() == 0);
+            if (shcarb1)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb1, codeForCarb1));
+            }
+
+
 
             string typeForCarb2 = MyFoodWeek.thisweek.FoodCategoryTypeCarb2;
             string codeForCarb2 = MyFoodWeek.thisweek.FoodCategoryCodeCarb2;
             string descForCarb2 = MyFoodWeek.thisweek.FoodCategoryDescCarb2;
-            FoodItem.InsertFoodItem(typeForCarb2, codeForCarb2, descForCarb2, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb2, codeForCarb2));
+            //FoodItem.InsertFoodItem(typeForCarb2, codeForCarb2, descForCarb2, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb2, codeForCarb2));
+            //Si n'est pas présent dans food item
+            var resultcarb2 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb2)));
+            bool car2 = (resultcarb2.Count() == 0);
+            if (car2)
+            {
+                FoodItem.InsertFoodItem(typeForCarb2, codeForCarb2, descForCarb2, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var rescarb2 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb2)));
+            bool shcarb2 = (rescarb2.Count() == 0);
+            if (shcarb2)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb2, codeForCarb2));
+            }
+
 
 
             string typeForCarb3 = MyFoodWeek.thisweek.FoodCategoryTypeCarb3;
             string codeForCarb3 = MyFoodWeek.thisweek.FoodCategoryCodeCarb3;
             string descForCarb3 = MyFoodWeek.thisweek.FoodCategoryDescCarb2;
-            FoodItem.InsertFoodItem(typeForCarb3, codeForCarb3, descForCarb3, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb3, codeForCarb3));
+            //FoodItem.InsertFoodItem(typeForCarb3, codeForCarb3, descForCarb3, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb3, codeForCarb3));
+
+            //Si n'est pas présent dans food item
+            var resultcarb3 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb3)));
+            bool car3 = (resultcarb3.Count() == 0);
+            if (car3)
+            {
+                FoodItem.InsertFoodItem(typeForCarb3, codeForCarb3, descForCarb3, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var rescarb3 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeCarb3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeCarb3)));
+            bool shcarb3 = (rescarb3.Count() == 0);
+            if (shcarb3)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForCarb3, codeForCarb3));
+            }
+
+            //__________________________________________________________________
+            //_____________________ Prot _______________________________________
 
             string typeForProt1 = MyFoodWeek.thisweek.FoodCategoryTypeProt1;
             string codeForProt1 = MyFoodWeek.thisweek.FoodCategoryCodeProt1;
             string descForProt1 = MyFoodWeek.thisweek.FoodCategoryDescProt1;
-            FoodItem.InsertFoodItem(typeForProt1, codeForProt1, descForProt1, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt1, codeForProt1));
+            //FoodItem.InsertFoodItem(typeForProt1, codeForProt1, descForProt1, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt1, codeForProt1));
+
+            //Si n'est pas présent dans food item
+            var resultprot1 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt1)));
+            bool prot1 = (resultprot1.Count() ==0);
+            if (prot1)
+            {
+                FoodItem.InsertFoodItem(typeForProt1, codeForProt1, descForProt1, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resprot1 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt1)));
+            bool shprot1 = (resprot1.Count() == 0);
+            if (shprot1)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt1, codeForProt1));
+            }
+
 
             string typeForProt2 = MyFoodWeek.thisweek.FoodCategoryTypeProt2;
             string codeForProt2 = MyFoodWeek.thisweek.FoodCategoryCodeProt2;
             string descForProt2 = MyFoodWeek.thisweek.FoodCategoryDescProt2;
-           FoodItem.InsertFoodItem(typeForProt2, codeForProt2, descForProt2, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt2, codeForProt2));
+            //FoodItem.InsertFoodItem(typeForProt2, codeForProt2, descForProt2, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt2, codeForProt2));
+
+            //Si n'est pas présent dans food item
+            var resultprot2 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt2)));
+            bool prot2 = (resultprot2.Count() == 0);
+            if (prot2)
+            {
+                FoodItem.InsertFoodItem(typeForProt2, codeForProt2, descForProt2, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resprot2 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt2)));
+            bool shprot2 = (resprot2.Count() == 0);
+            if (shprot2)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt2, codeForProt2));
+            }
 
             string typeForProt3 = MyFoodWeek.thisweek.FoodCategoryTypeProt3;
             string codeForProt3 = MyFoodWeek.thisweek.FoodCategoryCodeProt3;
             string descForProt3 = MyFoodWeek.thisweek.FoodCategoryDescProt3;
-            FoodItem.InsertFoodItem(typeForProt3, codeForProt3, descForProt3, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt3, codeForProt3));
+            //FoodItem.InsertFoodItem(typeForProt3, codeForProt3, descForProt3, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt3, codeForProt3));
+
+            //Si n'est pas présent dans food item
+            var resultprot3 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt3)));
+            bool prot3 = (resultprot3.Count() == 0);
+            if (prot3)
+            {
+                FoodItem.InsertFoodItem(typeForProt3, codeForProt3, descForProt3, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resprot3 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeProt3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeProt3)));
+            bool shprot3 = (resprot3.Count() == 0);
+            if (shprot3)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForProt3, codeForProt3));
+            }
+
+            //_________________________________________________________________________________
+            //_________________ Veggie ________________________________________________________
 
             string typeForVeggie1 = MyFoodWeek.thisweek.FoodCategoryTypeVeggies1;
             string codeForVeggie1 = MyFoodWeek.thisweek.FoodCategoryCodeVeggies1;
             string descForVeggie1 = MyFoodWeek.thisweek.FoodCategoryDescVeggies1;
-            FoodItem.InsertFoodItem(typeForVeggie1, codeForVeggie1, descForVeggie1, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie1, codeForVeggie1));
+            //FoodItem.InsertFoodItem(typeForVeggie1, codeForVeggie1, descForVeggie1, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie1, codeForVeggie1));
+
+            //Si n'est pas présent dans food item
+            var resultveggie1 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies1)));
+            bool veggie1 = (resultveggie1.Count() == 0);
+            if (veggie1)
+            {
+                FoodItem.InsertFoodItem(typeForVeggie1, codeForVeggie1, descForVeggie1, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resveggie1 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies1), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies1)));
+            bool shveggie1 = (resveggie1.Count() == 0);
+            if (shveggie1)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie1, codeForVeggie1));
+            }
 
             string typeForVeggie2 = MyFoodWeek.thisweek.FoodCategoryTypeVeggies2;
             string codeForVeggie2 = MyFoodWeek.thisweek.FoodCategoryCodeVeggies2;
             string descForVeggie2 = MyFoodWeek.thisweek.FoodCategoryDescVeggies2;
-            FoodItem.InsertFoodItem(typeForVeggie2, codeForVeggie2, descForVeggie2, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie2, codeForVeggie2));
+            //FoodItem.InsertFoodItem(typeForVeggie2, codeForVeggie2, descForVeggie2, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie2, codeForVeggie2));
+            //Si n'est pas présent dans food item
+            var resultveggie2 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies2)));
+            bool veggie2 = (resultveggie2.Count() == 0);
+            if (veggie2)
+            {
+                FoodItem.InsertFoodItem(typeForVeggie2, codeForVeggie2, descForVeggie2, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resveggie2 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies2), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies2)));
+            bool shveggie2 = (resveggie2.Count() == 0);
+            if (shveggie2)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie2, codeForVeggie2));
+            }
+
 
             string typeForVeggie3 = MyFoodWeek.thisweek.FoodCategoryTypeVeggies3;
             string codeForVeggie3 = MyFoodWeek.thisweek.FoodCategoryCodeVeggies3;
             string descForVeggie3 = MyFoodWeek.thisweek.FoodCategoryDescVeggies3;
-            FoodItem.InsertFoodItem(typeForVeggie3, codeForVeggie3, descForVeggie3, res);
-            col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie3, codeForVeggie3));
+            //FoodItem.InsertFoodItem(typeForVeggie3, codeForVeggie3, descForVeggie3, res);
+            //col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie3, codeForVeggie3));
 
+            //Si n'est pas présent dans food item
+            var resultveggie3 = col2.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies3)));
+            bool veggie3 = (resultveggie3.Count() == 0);
+            if (veggie3)
+            {
+                FoodItem.InsertFoodItem(typeForVeggie3, codeForVeggie3, descForVeggie3, res);
+            }
+            //Si n'est pas présent dans food pou liste de courses
+            var resveggie3 = col.Find(Query.And(Query.EQ("NameCode", MyFoodWeek.thisweek.FoodCategoryCodeVeggies3), Query.EQ("Type", MyFoodWeek.thisweek.FoodCategoryTypeVeggies3)));
+            bool shveggie3 = (resveggie3.Count() == 0);
+            if (shveggie3)
+            {
+                col.Insert(FoodItem.getEntryfromTypeAndCode(typeForVeggie3, codeForVeggie3));
+            }
 
         }
 
