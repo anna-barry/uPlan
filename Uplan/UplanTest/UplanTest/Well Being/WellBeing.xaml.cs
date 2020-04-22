5bacd8213c712d790c541c7e5c8af331f7620605
anna.barry@me.com <anna.barry@me.com>
2020-04-22 16:58:46 +0200

    partie rémi a des erreurs

--- Uplan/UplanTest/UplanTest/Well Being/WellBeing.xaml.cs
+++ Uplan/UplanTest/UplanTest/Well Being/WellBeing.xaml.cs
@@ -1,24 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Plugin.SimpleAudioPlayer;
+using SkiaSharp;
 using System.Text;
 using System.Threading.Tasks;
-
+using Syncfusion.Buttons;
 using Xamarin.Forms;
+
+using MediaManager;
+using MediaManager.Forms;
+
 using Xamarin.Forms.Xaml;
+using Syncfusion.XForms.Buttons;
+using Xamarin.Forms.PlatformConfiguration;
+using System.Text.RegularExpressions;
+using UplanTest;
+using MediaManager.Player;
+using System.ComponentModel;
+using MediaManager.Notifications;
+//using UplanTest.Well_Being;//
 
 namespace UplanTest
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WellBeing : ContentPage
+
+
     {
+
+        DateTime tps;
+
+
+
         public WellBeing()
         {
             InitializeComponent();
+
             //la faire un random pour trouver la quote du jour et la mettre a quote !;text breffff
             //var col= Database.db.GetCollection<ListEntry>("ListEntries");
             //var quotes = col.FindAll(Query.EQ("DueDate", Monday.Date));
 
+            //  Device.StartTimer(TimeSpan.FromSeconds(1),)//
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+
+
+
+
+
+
+
         }
+        DateTime actu = DateTime.Now;
+
+
+
+
+
+
+        bool OnTimerTick()
+        {
+            if (_switch.IsToggled && DateTime.Now >= tps)
+            {
+                _switch.IsToggled = false;
+                DisplayAlert("Timer Alert", "The '" + "'break'" + "' timer has elapsed", "OK");
+                var alert = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+
+                alert.Load("alert.mp3");
+                alert.Play();
+
+
+            }
+            return true;
+        }
+        void Event(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+                _switch.IsVisible = true;
+            }
+        }
+
+        void Pass(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+            Navigation.PushAsync(new musik());
+        }
+
+        void SetTriggerTime()
+        {
+            if (_switch.IsToggled)
+            {
+                tps = DateTime.Today + Chrono.Time;
+                if (tps < DateTime.Now)
+                {
+                    tps += TimeSpan.FromDays(1);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        public static void ToPause1(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+        private void ToPlay2(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+        private void ToPause2(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
     }
+
 }
---
+++ Uplan/UplanTest/UplanTest/Well Being/musik.xaml.cs
@@ -0,0 +1,220 @@
+﻿using MediaManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lottie.Forms;
+using Lottie;
+
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.SimpleAudioPlayer;
+using System.Runtime.InteropServices;
+
+namespace UplanTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class musik : ContentPage
+    {
+
+
+        public musik()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+
+        }
+
+
+        private void ToBack(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new WellBeing());
+        }
+        public ISimpleAudioPlayer p1 = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+
+        private async void ToPlay1(object sender, EventArgs e)
+        {
+
+            p1.Load("jazz.mp3");
+
+            p1.Play();
+            P0.IsVisible = true;
+            await P0.ProgressTo(1, 200000, Easing.Linear);
+
+
+
+
+
+
+
+
+        }
+        private void ToPause1(object sender, EventArgs e)
+        {
+
+            p1.Pause();
+            P0.IsVisible = false;
+
+
+
+
+
+
+        }
+        private async void ToPlay2(object sender, EventArgs e)
+        {
+            p1.Load("relax2.mp3");
+            p1.Play();
+            P1.IsVisible = true;
+
+            await P1.ProgressTo(1, 290000, Easing.Linear);
+
+
+
+        }
+        private void ToPause2(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P1.IsVisible = false;
+
+
+
+
+        }
+        private async void ToPlay3(object sender, EventArgs e)
+        {
+
+            p1.Load("relax3.mp3");
+            p1.Play();
+            P2.IsVisible = true;
+
+            await P2.ProgressTo(1, 218000, Easing.Linear);
+
+
+
+
+
+        }
+        private void ToPause3(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P2.IsVisible = false;
+
+
+
+
+        }
+        private async void ToPlay4(object sender, EventArgs e)
+        {
+
+            p1.Load("relax4.mp3");
+            p1.Play();
+            P3.IsVisible = true;
+            await P3.ProgressTo(1, 290000, Easing.Linear);
+
+
+
+        }
+        private void ToPause4(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P3.IsVisible = false;
+
+
+
+
+        }
+        private async void ToPlay5(object sender, EventArgs e)
+        {
+
+            p1.Load("rock1.mp3");
+            p1.Play();
+            P4.IsVisible = true;
+            await P4.ProgressTo(1, 138000, Easing.Linear);
+
+
+
+        }
+        private void ToPause5(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P4.IsVisible = false;
+
+
+
+
+        }
+        private async void ToPlay6(object sender, EventArgs e)
+        {
+
+            p1.Load("rock2.mp3");
+            p1.Play();
+            P5.IsVisible = true;
+            await P5.ProgressTo(1, 231000, Easing.Linear);
+
+
+
+        }
+        private void ToPause6(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P5.IsVisible = false;
+
+
+
+
+        }
+        private async void ToPlay7(object sender, EventArgs e)
+        {
+
+            p1.Load("rock3.mp3");
+            p1.Play();
+            P6.IsVisible = true;
+            await P6.ProgressTo(1, 337000, Easing.Linear);
+
+
+
+
+        }
+        private void ToPause7(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P6.IsVisible = false;
+
+
+
+
+
+        }
+        private async void ToPlay8(object sender, EventArgs e)
+        {
+
+            p1.Load("rock4.mp3");
+            p1.Play();
+            P7.IsVisible = true;
+            await P7.ProgressTo(1, 198000, Easing.Linear);
+
+
+
+        }
+        private void ToPause8(object sender, EventArgs e)
+        {
+            p1.Pause();
+            P7.IsVisible = false;
+
+
+
+
+
+        }
+    }
+}
