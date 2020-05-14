f89d98565a076962c335c97c6815551fc656336c
anna.barry@me.com <anna.barry@me.com>
2020-05-14 15:12:31 +0200

    2nd version of Sport

--- Uplan/UplanTest/UplanTest/Sport/SchedWorkout.xaml.cs
+++ Uplan/UplanTest/UplanTest/Sport/SchedWorkout.xaml.cs
@@ -29,6 +29,28 @@ namespace UplanTest
             Arms4.Content = ForArms4;
             Arms5.Content = ForArms5;
             Arms6.Content = ForArms6;
+            Leg1.Content = ForLeg1;
+            Leg2.Content = ForLeg2;
+            Leg3.Content = ForLeg3;
+            Leg4.Content = ForLeg4;
+            Leg5.Content = ForLeg5;
+            Leg6.Content = ForLeg6;
+            Glutes1.Content = ForGlutes1;
+            Glutes2.Content = ForGlutes2;
+            Glutes3.Content = ForGlutes3;
+            Glutes4.Content = ForGlutes4;
+            Glutes5.Content = ForGlutes5;
+            Glutes6.Content = ForGlutes6;
+            FullBody1.Content = ForFullBody1;
+            FullBody2.Content = ForFullBody2;
+            FullBody3.Content = ForFullBody3;
+            FullBody4.Content = ForFullBody4;
+            FullBody5.Content = ForFullBody5;
+            FullBody6.Content = ForFullBody6;
+            //___________________________________________________________
+
+
+
         }
 
         private async void OnCloseClicked2(object sender, EventArgs args)
@@ -38,6 +60,7 @@ namespace UplanTest
 
         }
 
+        //_________________ ABS
         private async void GetInfoAbs1(object sender, EventArgs args)
         {
 
@@ -116,6 +139,8 @@ namespace UplanTest
 
         }
 
+
+        //__________ Arms
         private async void GetInfoArms1(object sender, EventArgs args)
         {
 
@@ -193,5 +218,245 @@ namespace UplanTest
 
 
         }
+
+        //_________________ Leg
+        private async void GetInfoLeg1(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg1(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoLeg2(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg2(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoLeg3(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg3(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoLeg4(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg4(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoLeg5(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg5(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoLeg6(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddLeg6(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        //__________________ Booty 
+        private async void GetInfoGlutes1(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes1(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoGlutes2(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes2(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoGlutes3(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes3(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoGlutes4(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes4(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoGlutes5(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes5(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoGlutes6(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddGlutes6(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        //__________ Full Body
+        private async void GetInfoFullBody1(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody1(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoFullBody2(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody2(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoFullBody3(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody3(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoFullBody4(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody4 (object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoFullBody5(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody5(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+        private async void GetInfoFullBody6(object sender, EventArgs args)
+        {
+
+
+        }
+
+        private async void AddFullBody6(object sender, EventArgs args)
+        {
+
+
+
+        }
+
+
+
     }
 }
---
+++ Uplan/UplanTest/UplanTest/Sport/Workout.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UplanTest.Sport
+{
+    class Workout
+    {
+    }
+}
