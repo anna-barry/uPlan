bfeb4c2e1ec0a674ffbe9d701d99c289d2d023c2
Marine Thunet <marine.thunet@epita.fr>
2020-05-18 18:47:57 +0200

    SportCHRONO

--- Uplan/UplanTest/UplanTest/Sport/StartWorkout.xaml.cs
+++ Uplan/UplanTest/UplanTest/Sport/StartWorkout.xaml.cs
@@ -12,8 +12,6 @@ namespace UplanTest
     public partial class StartWorkout : ContentPage
     {
         private Label LilTime = new Label();
-        private Timer _timer;
-        private int _countSeconds;
         public StartWorkout(DateTime day)
         {
             InitializeComponent();
@@ -38,63 +36,73 @@ namespace UplanTest
                                 if (!pause)
                                 {
                                     Type.Text = listOfWorkouts.ElementAt(kactual-1).Type;
-                                LilTime.Text = (30 - seconds).ToString();
+                                LilTime.Text =(30 - seconds).ToString();
                                 LilTime.HorizontalOptions = LayoutOptions.Center;
                                 LilTime.VerticalOptions = LayoutOptions.Center;
-                                LilTime.FontSize = 20;
+                                LilTime.FontSize = 70;
 
                                 switch (ActExercice)
                                 {
                                     case 1:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice1.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice1.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice1.Code; }
                                         break;
                                     case 2:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice2.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice2.Code;
+                                            if (seconds== 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice2.Code; }
                                         break;
                                     case 3:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice3.Description;
-                                            Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice3.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice3.Code; }
                                             break;
                                     case 4:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice4.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice4.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice4.Code; }
                                         break;
                                     case 5:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice5.Description;
-                                            Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice5.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice5.Code; }
                                             break;
                                     case 6:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice6.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice6.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice6.Code; }
                                         break;
                                     case 7:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice7.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice7.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice7.Code; }
                                         break;
                                     case 8:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice8.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice8.Code;
+                                            if (seconds== 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice8.Code; }
                                         break;
                                     case 9:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice9.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice9.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source =listOfWorkouts.ElementAt(kactual - 1).Exercice9.Code; }
                                         break;
                                     case 10:
                                         Exerice.Text = listOfWorkouts.ElementAt(kactual - 1).Exercice10.Description;
-                                        Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice10.Code;
+                                            if (seconds == 0)
+                                            { Picture.Source = listOfWorkouts.ElementAt(kactual - 1).Exercice10.Code; }
                                         break;
                                 }
 
 
                                 InTheFrame.Content = LilTime;
-                                if (seconds >= 25)
+                                if (seconds == 25)
                                 {InTheFrame.BackgroundColor = Color.IndianRed;}
-                                else
+                                if (seconds%30==1)
                                 {InTheFrame.BackgroundColor = Color.LightGreen;}
 
-                                if (seconds == 30 && ActExercice == 10 && round == 1)
+                                if ( seconds==30 && ActExercice == 10 && round == 1)
                                 {
                                     round = 2;
                                     seconds = 0;
@@ -102,7 +110,7 @@ namespace UplanTest
                                 }
                                 else
                                 {
-                                    if (!(seconds == 30 && kactual == nb_workouts && round == 2 && ActExercice == 10))
+                                    if (!(seconds== 30 && kactual == nb_workouts && round == 2 && ActExercice == 10))
                                     {
                                         if (seconds == 30)
                                         {
@@ -148,7 +156,7 @@ namespace UplanTest
                                 }
                             });
 
-                            if (seconds == 30 && kactual == nb_workouts && round == 2 && ActExercice == 10)
+                            if (seconds >= 30*nb_workouts*2*10 && kactual == nb_workouts && round == 2 && ActExercice == 10)
                             {
                                 Type.Text = "Well done";
                                 Exerice.Text = "You have finished your workout, you should be proud of yourself";
@@ -160,7 +168,7 @@ namespace UplanTest
                             return true; // runs again, or false to stop
                         });
 
-                    }
+        }
 
         private async void OnCloseClicked2(object sender, EventArgs args)
         {
